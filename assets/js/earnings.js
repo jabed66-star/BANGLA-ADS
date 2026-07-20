fce0025352766eab8183de3727efcadda89307be
jabed66-star <jabedmahmud66@gmail.com>
2026-07-21 01:34:27 +0600

    Create earnings.js

---
+++ assets/js/earnings.js
@@ -0,0 +1,6 @@
+// assets/js/earnings.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Earnings পেজের জন্য কোনো কাস্টম ইন্টারেকশন বা ডাটা ফিল্টারিং লাগলে এখানে যোগ করা যাবে
+    console.log("Earnings page loaded successfully.");
+});
